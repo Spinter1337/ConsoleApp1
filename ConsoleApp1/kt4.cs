0c4e4b629e0282855cd7c5392f93810d42c01adb
Spinter1337 <shakirov2019@mail.ru>
2022-10-18 15:42:29 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/kt4.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Room r1 = new Room();
+Room r2 = new Room(20);
+Room r3 = new Room(r2);
+
+r1.Show();
+r2.Show();
+r3.Show();
+class Room
+{
+    private int number;
+    public void Show()
+    {
+        Console.WriteLine(number);
+    }
+    public Room()
+    {
+        Random rnd = new Random();
+        number = rnd.Next();
+    }
+    public Room(int number)
+    {
+        this.number = number;
+    }
+    public Room(Room room)
+    {
+        this.number = room.number;
+    }
+}
